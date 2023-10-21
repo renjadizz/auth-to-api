e0ccd42b344f0bb867168cd584f321180f6b81c8
Murat <renja.dizz@gmail.com>
2023-10-22 00:54:15 +0600

    add signup function

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # auth-to-api
- 
+# api: https://api.realworld.io/api/ 
--- main.js
+++ main.js
@@ -1,6 +1,44 @@
 window.onload = (event) => {
+    let formSignup = document.querySelector(".signup-container__form");
+    formSignup.onsubmit = async function (event) {
+        event.preventDefault();
+        let serverInfo = document.querySelector(".server-info");
+        clearServerInfo(serverInfo);
+        let formData = new FormData(formSignup);
+        let formDataObject = {};
+        formData.forEach((value, key) => formDataObject[key] = value);
+        let sendData = {user: formDataObject};
+        let jsonSend = JSON.stringify(sendData);
+        try {
+            const fetchResult = await fetch("https://api.realworld.io/api/users",
+                {
+                    method: "POST",
+                    body: jsonSend,
+                    headers: {
+                        "Content-type": "application/json"
+                    }
+                });
+            const jsonData = await fetchResult.json();
+            let pServerInfo = document.createElement("p");
+            pServerInfo.setAttribute("class", "server-info__p-success");
+            pServerInfo.textContent = `${jsonData.user.username} was created. Now you can Log in`;
+            serverInfo.appendChild(pServerInfo);
+        } catch (err) {
+            let pServerInfo = document.createElement("p");
+            pServerInfo.setAttribute("class", "server-info__p-fail");
+            pServerInfo.textContent = `Something went wrong. Try again later\n${err.message}`;
+            serverInfo.appendChild(pServerInfo);
+        }
 
+    }
+
+    function clearServerInfo(serverInfo) {
+        while (serverInfo.firstChild) {
+            serverInfo.removeChild(serverInfo.firstChild);
+        }
+    }
 }
+
 function signup() {
     document.querySelector(".login-container").style.cssText = "display: none;";
     document.querySelector(".signup-container").style.cssText = "display: block;";
